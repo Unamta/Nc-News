3552e1f548b39551bc63dc5cc4946e3eb2941af0
Unamta Aslam <aslamunamta@gmail.com>
2025-03-10 16:20:04 +0000

    Finish GET /api endpoint

---
+++ controllers/api.controllers.js
@@ -0,0 +1,9 @@
+const endpoints = require("../endpoints.json")
+
+function getEndpoints(request, response) {
+  response
+    .status(200)
+    .send({ endpoints: endpoints })
+}
+
+module.exports = { getEndpoints };
